e500e04603b1b85e46554e743eb7d7e660250a1d
Bastan <bastaw1@medtronic.com>
2023-10-20 15:38:19 -0400

    finished auth

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const keys = require("../config/keys");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +24,21 @@ passport.use(
     },
 
     (accessToken, refreshToken, profile, done) => {
-      console.log("access Token: ", accessToken),
-        console.log("refresh Token: ", refreshToken),
-        console.log(profile);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // we already have a record
+          done(null, existingUser);
+        } else {
+          //if user does not exist, create the user
+          new User({
+            googleId: profile.id,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+          })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
     }
   )
 );
